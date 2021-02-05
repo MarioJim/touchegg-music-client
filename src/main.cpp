18dc7eb25c2641042d658fc9a9b05b12c498ad9c
MarioJim <mario.emilio.j@gmail.com>
2021-02-05 15:43:33 -0600

    Fetch touchegg sources using CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "actions/action-type.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << actionTypeToStr(ActionType::FULLSCREEN_WINDOW) << "\n";
+
     return 0;
 }
