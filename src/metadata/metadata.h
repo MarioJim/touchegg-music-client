d0bc297cc65982f5b86680aa1a270c7925ede007
MarioJim <mario.emilio.j@gmail.com>
2021-04-03 21:51:25 -0600

    [wip] Display album icon

--- src/metadata/metadata.h
+++ src/metadata/metadata.h
@@ -1,6 +1,8 @@
 #ifndef TOUCHEGG_MUSIC_CLIENT_METADATA_H
 #define TOUCHEGG_MUSIC_CLIENT_METADATA_H
 
+#include <gdk-pixbuf/gdk-pixbuf.h>
+
 #include <iostream>
 #include <string>
 #include <tuple>
@@ -11,13 +13,15 @@
 struct Metadata {
   const std::string song, album, artist;
   const PlaybackStatus playback_status;
+  GdkPixbuf* album_icon;
 
   Metadata(std::string song, std::string album, std::string artist,
-           PlaybackStatus status)
+           PlaybackStatus status, GdkPixbuf* album_icon)
       : song(std::move(song)),
         album(std::move(album)),
         artist(std::move(artist)),
-        playback_status(status) {}
+        playback_status(status),
+        album_icon(album_icon) {}
 };
 
 #endif  // TOUCHEGG_MUSIC_CLIENT_METADATA_H
