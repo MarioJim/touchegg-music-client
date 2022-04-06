31b0346c2ef9bf0b163d9e309f373e55fbb3c973
MarioJim <mario.emilio.j@gmail.com>
2022-04-06 01:56:13 -0500

    Update touchegg dependency

--- cmake/touchegg-dependency.cmake
+++ cmake/touchegg-dependency.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(touchegg
         GIT_REPOSITORY https://github.com/JoseExposito/touchegg.git
-        GIT_TAG 2.0.9
+        GIT_TAG 2.0.14
         GIT_SHALLOW ON
         GIT_PROGRESS ON
         )
